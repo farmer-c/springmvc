da412d6d88a1b4bdea91470f2a4ddcfe5454e98c
farmer-k <farmer.ong@gmail.com>
2023-10-04 18:01:23 +0900

    잘못 Override 된 서블릿 메소드 정정

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,17 +1,17 @@
 package hello.servlet.basic;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
   @Override
-  public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+  protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     System.out.println("HelloServlet.service");
     System.out.println("request = " + request);
     System.out.println("response = " + response);
